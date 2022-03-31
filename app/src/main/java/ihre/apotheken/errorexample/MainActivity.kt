bdd6eef4efd5d57bbef38fa9cf466b5bbf0f27dc
ljmatan <73232684+ljmatan@users.noreply.github.com>
2022-03-31 16:33:35 +0200

    Remove unused method

--- app/src/main/java/ihre/apotheken/errorexample/MainActivity.kt
+++ app/src/main/java/ihre/apotheken/errorexample/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar!!.hide()
         setNavigationView()
     }
 }
